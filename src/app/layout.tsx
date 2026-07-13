99d68201b5ca54fcebea040448981d9cdb246447
Deploy <deploy@example.com>
2026-07-13 07:31:32 -0300

    chore: atualiza tag de verificacao do google search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Gestor de Documentos",
   description: "Plataforma centralizada para Gestão de Documentos e Processos.",
   verification: {
-    google: "lOaGb6Cq8m7mCzHHuQ3rysBg7nagqcQcxSppOMpa0DI",
+    google: "vHE5a4RY1pUqcipro_o4l6ByIIYBAO7U5JDdpMzueT0",
   },
 };
 
@@ -38,7 +38,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <meta name="google-site-verification" content="lOaGb6Cq8m7mCzHHuQ3rysBg7nagqcQcxSppOMpa0DI" />
+        <meta name="google-site-verification" content="vHE5a4RY1pUqcipro_o4l6ByIIYBAO7U5JDdpMzueT0" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
